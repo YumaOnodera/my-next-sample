1328fae5cd7b0b367e3f685e6d3810c793021bf9
小野寺勇磨 <yuma.onodera0913@gmail.com>
2022-08-12 19:51:01 +0900

    ダッシュボード画面実装

---
+++ src/components/Layouts/AppLayout.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+import { useAuth } from 'hooks/auth'
+
+const AppLayout: React.FC<{ header: ReactNode, children: ReactNode }> = ({ header, children }) => {
+    const { user } = useAuth({ middleware: 'auth' })
+
+    return (
+        <div>
+            {/* Page Heading */}
+            <header>
+                <div>
+                    {header}
+                </div>
+            </header>
+
+            {/* Page Content */}
+            <main>
+                {children}
+                { user?.name }
+                { user?.email }
+            </main>
+        </div>
+    )
+}
+
+export default AppLayout
