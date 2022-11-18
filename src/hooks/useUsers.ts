30d4e876d70c63449657bc6ef1182b15265c6b30
小野寺勇磨 <yuma.onodera0913@gmail.com>
2022-11-18 15:42:18 +0900

    不要な記述を削除

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,4 +1,3 @@
-import { useCsrf } from "hooks/useCsrf";
 import { useFormat } from "hooks/useFormat";
 import { useUser } from "hooks/useUser";
 import axios from "libs/axios";
@@ -6,13 +5,10 @@ import axios from "libs/axios";
 import type { UpdateUser } from "types/users";
 
 export const useUsers = () => {
-  const csrf = useCsrf();
   const { objectValuesToString } = useFormat();
   const { mutate } = useUser();
 
   const update: UpdateUser = async ({ setErrors, ...props }) => {
-    await csrf();
-
     setErrors([]);
 
     axios
